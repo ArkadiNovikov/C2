33ca7b14d2b81d5b42cbeff95b3dc64144684027
Arkadi Novikov <arkadi.novikov@outlook.com>
2017-03-28 04:06:16 +0900

    整形

--- jax-rs/src/main/java/com/untitleddoc/cadencecalc/jaxrs/CranksetService.java
+++ jax-rs/src/main/java/com/untitleddoc/cadencecalc/jaxrs/CranksetService.java
@@ -5,7 +5,6 @@
  */
 package com.untitleddoc.cadencecalc.jaxrs;
 
-import com.google.common.collect.Lists;
 import com.untitleddoc.cadencecalc.jaxrs.models.Crankset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,120 +22,122 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("crankset")
 @Stateless
-public class CranksetService {
+public class CranksetService
+{
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<com.untitleddoc.cadencecalc.jaxrs.models.Crankset> getCrankset() {
-        final List<Crankset> list = new ArrayList<>();
-        {
-            final List<Integer> tooths = Arrays.asList(34, 50);
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<com.untitleddoc.cadencecalc.jaxrs.models.Crankset> getCrankset()
+	{
+		final List<Crankset> list = new ArrayList<>();
+		{
+			final List<Integer> tooths = Arrays.asList(34, 50);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(36, 52);
+			final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(36, 52);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(38, 52);
+			final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(38, 52);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(39, 53);
+			final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(39, 53);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(36, 46);
+			final Crankset item = new Crankset("FC-9000", "Shimano", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(36, 46);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("FC-6800", "Shimano", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(36, 48);
+			final Crankset item = new Crankset("FC-6800", "Shimano", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(36, 48);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(34, 48);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(34, 48);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(32, 48);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(32, 48);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(34, 46);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(34, 46);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(32, 46);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(32, 46);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(30, 46);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(30, 46);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(36, 44);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(36, 44);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(34, 44);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(34, 44);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(32, 44);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(32, 44);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(30, 44);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(30, 44);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("OX901D", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(26, 36, 48);
+			final Crankset item = new Crankset("OX901D", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(26, 36, 48);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("ALPINA2 Triple", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(26, 36, 46);
+			final Crankset item = new Crankset("ALPINA2 Triple", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(26, 36, 46);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("ALPINA2 Triple", "Sugino", tooths);
-            list.add(item);
-        }
-        {
-            final List<Integer> tooths = Arrays.asList(22, 33, 40);
+			final Crankset item = new Crankset("ALPINA2 Triple", "Sugino", tooths);
+			list.add(item);
+		}
+		{
+			final List<Integer> tooths = Arrays.asList(22, 33, 40);
 			tooths.sort(Comparator.reverseOrder());
-            final Crankset item = new Crankset("FC-M9000-3", "Shimano", tooths);
-            list.add(item);
-        }
-        return list;
-    }
+			final Crankset item = new Crankset("FC-M9000-3", "Shimano", tooths);
+			list.add(item);
+		}
+		return list;
+	}
 }
--- jax-rs/src/main/java/com/untitleddoc/cadencecalc/jaxrs/PerimeterService.java
+++ jax-rs/src/main/java/com/untitleddoc/cadencecalc/jaxrs/PerimeterService.java
@@ -20,41 +20,47 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("perimeter")
 @Stateless
-public class PerimeterService {
+public class PerimeterService
+{
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Perimeter> getPerimeter() {
-        final List<Perimeter> list = new ArrayList<>();
-        {
-            final Perimeter item1 = new Perimeter("23-622", "700*23C", 2096);
-            list.add(item1);
-        }
-        {
-            final Perimeter item1 = new Perimeter("25-622", "700*25C", 2105);
-            list.add(item1);
-        }
-        {
-            final Perimeter item1 = new Perimeter("28-622", "700*28C", 2136);
-            list.add(item1);
-        }
-        {
-            final Perimeter item1 = new Perimeter("40-584", "27.5*1.50", 2079);
-            list.add(item1);
-        }
-        {
-            final Perimeter item1 = new Perimeter("25-584", "650*25B", 1991);
-            list.add(item1);
-        }
-        {
-            final Perimeter item1 = new Perimeter("23-571", "650x23C", 1944);
-            list.add(item1);
-        }
-        {
-            final Perimeter item1 = new Perimeter("25-571", "650x25C", 1952);
-            list.add(item1);
-        }
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Perimeter> getPerimeter()
+	{
+		final List<Perimeter> list = new ArrayList<>();
+		{
+			final Perimeter item1 = new Perimeter("23-622", "700*23C", 2096);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("25-622", "700*25C", 2105);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("28-622", "700*28C", 2136);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("25-584", "650*25B", 1991);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("40-584", "27.5*1.50", 2079);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("47-584", "650*47", 2136);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("23-571", "650x23C", 1944);
+			list.add(item1);
+		}
+		{
+			final Perimeter item1 = new Perimeter("25-571", "650x25C", 1952);
+			list.add(item1);
+		}
 
-        return list;
-    }
+		return list;
+	}
 }
